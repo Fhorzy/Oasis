1042938fedcf2afbf4a0fe94a126c526a6af6a31
Efendi <fndsanto@gmail.com>
2021-06-10 21:57:34 +0700

    add RegisterForm

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginForm from './src/components/LoginForm';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './src/components/LoginForm';
+import RegisterPage from './src/components/RegisterForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LoginForm/>
+     <Router>
+       <Switch>
+         <Route exact path='/login' component={LoginPage} />
+         <Route exact path='/register' component={RegisterPage} />
+       </Switch>
+     </Router>
+     <RegisterForm/>
       <StatusBar style="auto" />
     </View>
-    
   );
 }
 
